f5b1861256efbafe89852ebd397683cdd7db0c84
Hongge Wang <hongge.wang@stud.ki.se>
2024-03-22 07:56:10 +0100

    -

--- src/pages/Animation.js
+++ src/pages/Animation.js
@@ -1,6 +1,7 @@
 import Title from "../components/common/Title";
 import Back from "../components/common/Back"
 import Animation1 from "../components/animation/Animation1";
+import Animation2 from "../components/animation/Animation2";
 
 export default function Animation(){
     return(
@@ -8,6 +9,7 @@ export default function Animation(){
             <Title title="Animation"/>
             <Back/>
             <Animation1 />
+            <Animation2 />
 
         </div>
     )
